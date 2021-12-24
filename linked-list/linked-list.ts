88c1c49f4a4a6c8acad340cc76e4d8954f9deb5a
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2021-12-24 16:11:37 -0300

    add linked-list exercise

---
+++ linked-list/linked-list.ts
@@ -0,0 +1,25 @@
+export class LinkedList<TElement> {
+  push(element: unknown) {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  pop(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  shift(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  unshift(element: unknown) {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  delete(element: unknown) {
+    throw new Error('Remove this statement and implement this function')
+  }
+
+  count(): unknown {
+    throw new Error('Remove this statement and implement this function')
+  }
+}
